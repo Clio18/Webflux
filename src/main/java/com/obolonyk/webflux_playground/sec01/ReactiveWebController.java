5907751e0d30a9a98726f8976f9fcf4293dc0c19
U0L9511 <estclio09@gmail.com>
2024-07-23 15:46:35 +0200

    - resiliency

--- src/main/java/com/obolonyk/webflux_playground/sec01/ReactiveWebController.java
+++ src/main/java/com/obolonyk/webflux_playground/sec01/ReactiveWebController.java
@@ -29,11 +29,18 @@ public class ReactiveWebController {
                 // -N flag turn off the buffering and when request terminates the data will be stop collecting
 
                 //if the page refreshes, the request will be STOPs and sent again
+
+                //if the app from where we retrieve the data crashes, the portion of the dara will return
+                // RESILIENT
+                .onErrorComplete()
                         .doOnNext(product -> log.info("Product: {}", product));
     }
 
 
     // will be showing product by product without waiting for all products to be collected
+
+    // the returning type does matter, when the browser make a request it be checking the type of the response
+    // and then subscription will be created
     @GetMapping(value = "/products/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Product> getProductsStream() {
         return this.webClient.get()
