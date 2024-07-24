103a87905accc133b45685e23a128ed34e33792c
U0L9511 <estclio09@gmail.com>
2024-07-24 09:41:50 +0200

    - enable showing sql in logs

--- src/test/java/com/obolonyk/webflux_playground/sec02/AbstractTest.java
+++ src/test/java/com/obolonyk/webflux_playground/sec02/AbstractTest.java
@@ -3,6 +3,9 @@ package com.obolonyk.webflux_playground.sec02;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "sec=sec02")
+@SpringBootTest(properties = {
+        "sec=sec02",
+        "logging.level.org.springframework.r2dbc=DEBUG"
+})
 public abstract class AbstractTest {
 }
