25816a733869ef23adf8a77abe6907d77942db99
U0L9511 <estclio09@gmail.com>
2024-07-26 21:19:19 +0200

    - check concurrent

--- src/test/java/com/obolonyk/webflux_playground/sec07/Lec01MonoTest.java
+++ src/test/java/com/obolonyk/webflux_playground/sec07/Lec01MonoTest.java
@@ -7,16 +7,31 @@ import java.time.Duration;
 
 public class Lec01MonoTest extends AbstractWebclient {
     private final WebClient webClient = createWebClient();
+
     @Test
-        void getProduct() throws InterruptedException {
+    void getProduct() throws InterruptedException {
+        webClient.get()
+                .uri("/lec01/product/1")
+                .retrieve()
+                .bodyToMono(ProductDto.class)
+                .doOnNext(print())
+                .subscribe();
+
+        //we should block thread because result is async
+        Thread.sleep(Duration.ofSeconds(2));
+    }
+    @Test
+    void concurrent() throws InterruptedException {
+        for (int i = 0; i < 50; i++) {
             webClient.get()
-                    .uri("/lec01/product/1")
+                    .uri("/lec01/product/" + i)
                     .retrieve()
                     .bodyToMono(ProductDto.class)
                     .doOnNext(print())
                     .subscribe();
+        }
 
-            //we should block thread because result is async
-            Thread.sleep(Duration.ofSeconds(2));
+        //we should block thread because result is async
+        Thread.sleep(Duration.ofSeconds(2));
     }
 }
