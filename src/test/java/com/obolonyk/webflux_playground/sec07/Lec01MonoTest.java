08818ba1d4964307f53e8739a896b94d4261f0fc
U0L9511 <estclio09@gmail.com>
2024-07-26 20:58:58 +0200

    - add simple get request using webClient

---
+++ src/test/java/com/obolonyk/webflux_playground/sec07/Lec01MonoTest.java
@@ -0,0 +1,22 @@
+package com.obolonyk.webflux_playground.sec07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+
+public class Lec01MonoTest extends AbstractWebclient {
+    private final WebClient webClient = createWebClient();
+    @Test
+        void getProduct() throws InterruptedException {
+            webClient.get()
+                    .uri("/lec01/product/1")
+                    .retrieve()
+                    .bodyToMono(ProductDto.class)
+                    .doOnNext(print())
+                    .subscribe();
+
+            //we should block thread because result is async
+            Thread.sleep(Duration.ofSeconds(2));
+    }
+}
---
+++ src/test/java/com/obolonyk/webflux_playground/sec07/ProductDto.java
@@ -0,0 +1,7 @@
+package com.obolonyk.webflux_playground.sec07;
+
+public record ProductDto (
+        Integer id,
+        String description,
+        Integer price
+){}
