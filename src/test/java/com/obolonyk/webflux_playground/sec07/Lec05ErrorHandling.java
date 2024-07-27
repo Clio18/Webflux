44e623b11d62ad92d1b190a514d491237e8e3d5e
U0L9511 <estclio09@gmail.com>
2024-07-27 11:47:00 +0200

    - using exchange

--- src/test/java/com/obolonyk/webflux_playground/sec07/Lec05ErrorHandling.java
+++ src/test/java/com/obolonyk/webflux_playground/sec07/Lec05ErrorHandling.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class Lec05ErrorHandling extends AbstractWebclient{
@@ -24,7 +26,7 @@ public class Lec05ErrorHandling extends AbstractWebclient{
                 .doOnError(WebClientResponseException.class, ex-> {
                     log.info("{}", ex.getResponseBodyAs(ProblemDetail.class));
                 })
- 
+
                 //default value
                 //.onErrorReturn(new CalculatorResponse(0, 0, null, 0.0))
                 .onErrorReturn(
@@ -40,5 +42,28 @@ public class Lec05ErrorHandling extends AbstractWebclient{
                 .as(StepVerifier::create)
                 .expectComplete()
                 .verify();
+    }
+
+    @Test
+    void exchanger() {
+        webClient.get()
+                .uri("/lec05/calculator/{first}/{second}", 10, 20)
+                //exchange give as more info - full response, where we can check status or cookies
+                .exchangeToMono(this::decode )
+                .doOnNext(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    private Mono<CalculatorResponse> decode(ClientResponse response){
+        log.info("Status code {}", response.statusCode());
+        if(response.statusCode().is4xxClientError()){
+            return response.bodyToMono(ProblemDetail.class)
+                    .doOnNext(pd -> log.info("{}", pd))
+                    .then(Mono.empty());
+        }
+        return response.bodyToMono(CalculatorResponse.class);
     }
 }
