32f414c3d5fcf9a5cc136e7c3c8176803dfbd2e7
U0L9511 <estclio09@gmail.com>
2024-07-27 11:20:22 +0200

    - header tests options

--- src/test/java/com/obolonyk/webflux_playground/sec07/Lec04HeaderTest.java
+++ src/test/java/com/obolonyk/webflux_playground/sec07/Lec04HeaderTest.java
@@ -4,16 +4,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.test.StepVerifier;
 
+import java.util.Map;
+
 public class Lec04HeaderTest extends AbstractWebclient{
     private final WebClient webClient = createWebClient(
             b -> b.defaultHeader("caller-id", "order-service"));
 
     @Test
     void defaultHeader() {
+        var headers = Map.of(
+                "caller-id", "order-service",
+                "some-key", "some-value"
+        );
+
         webClient.get()
                 .uri("/lec04/product/{id}", 1 )
+
                 // to override header
                 //.header("caller-id", "new-value")
+                //.headers(h ->h.setAll(headers))
+
+
                 .retrieve()
                 .bodyToMono(ProductDto.class)
                 .doOnNext(print())
