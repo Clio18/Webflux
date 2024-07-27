6c479e4d66a981cde1f70cdbd2e3b2c5dcc623e2
U0L9511 <estclio09@gmail.com>
2024-07-27 12:27:42 +0200

    - use exchange filter for token generation

---
+++ src/test/java/com/obolonyk/webflux_playground/sec07/Lec09ExchangeFilter.java
@@ -0,0 +1,45 @@
+package com.obolonyk.webflux_playground.sec07;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
+
+import java.util.UUID;
+
+public class Lec09ExchangeFilter extends AbstractWebclient {
+    public static final Logger log = LoggerFactory.getLogger(Lec09ExchangeFilter.class);
+    private final WebClient webClient = createWebClient(b-> b.filter(tokenGeneration()));
+
+    @Test
+    void exchangeFilter() {
+        webClient.get()
+                .uri("/lec09/product/{id}", 1)
+                .retrieve()
+                .bodyToMono(ProductDto.class)
+                .doOnNext(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    // to generate new token
+    private ExchangeFilterFunction tokenGeneration() {
+        return (req, next) -> {
+            var token = UUID.randomUUID().toString().replace("-", "");
+            log.info("Generated Token: {}", token);
+            // we can not do like this because req is immutable
+            // req.headers().setBearerAuth(token);
+
+            ClientRequest newRequest = ClientRequest.from(req)
+                    .headers(h -> h.setBearerAuth(token))
+                    .build();
+            return next.exchange(newRequest);
+        };
+    }
+
+}
