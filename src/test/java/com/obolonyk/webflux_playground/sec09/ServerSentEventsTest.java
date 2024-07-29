183b7641e1bfc74a9f340d84bb0ce2705c4bc74f
U0L9511 <estclio09@gmail.com>
2024-07-29 17:04:29 +0200

    - add integration test

---
+++ src/test/java/com/obolonyk/webflux_playground/sec09/ServerSentEventsTest.java
@@ -0,0 +1,49 @@
+package com.obolonyk.webflux_playground.sec09;
+
+import com.obolonyk.webflux_playground.sec09.dto.ProductDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+
+@SpringBootTest(properties = "sec=sec09")
+@AutoConfigureWebTestClient
+public class ServerSentEventsTest {
+    public static final Logger log = LoggerFactory.getLogger(ServerSentEventsTest.class);
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void serverSentEvents(){
+        client.get()
+                .uri("/products/stream/80")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .returnResult(ProductDto.class)
+                .getResponseBody()
+                .take(3)
+                .doOnNext(p -> log.info("Product: {}", p))
+                .collectList()
+                .as(StepVerifier::create)
+                .assertNext(
+                        list -> {
+                            Assertions.assertEquals(3, list.size());
+                            Assertions.assertTrue(list.stream().allMatch(p -> p.price() <= 80));
+                        }
+                )
+                .expectComplete()
+                .verify();
+    }
+
+
+
+
+}
