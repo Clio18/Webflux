4ef5cc9a29dabde3e11be1cf3f85f66f00d9ca36
U0L9511 <estclio09@gmail.com>
2024-07-31 11:41:57 +0200

    - configuration of WebClient to increase capacity

--- src/test/java/com/obolonyk/webflux_playground/sec10/Lec01HttpConnectionPoolingTest.java
+++ src/test/java/com/obolonyk/webflux_playground/sec10/Lec01HttpConnectionPoolingTest.java
@@ -2,13 +2,33 @@ package com.obolonyk.webflux_playground.sec10;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 import reactor.test.StepVerifier;
 
-public class Lec01HttpConnectionPoolingTest extends AbstractWebclient{
-    private final WebClient client = createWebClient();
+public class Lec01HttpConnectionPoolingTest extends AbstractWebclient {
+    private final WebClient client = createWebClient(b -> {
+        var poolSize = 501;
+        // any name
+        var provider = ConnectionProvider.builder("my")
+                // queue to reuse connection
+                .lifo()
+                .maxConnections(poolSize)
+                // the size of the queue where requests will be placed and wait for connection
+                .pendingAcquireMaxCount(poolSize * 5)
+                .build();
+
+        var httpClient = HttpClient.create(provider)
+                // adjustment we need to add
+                .compress(true)
+                .keepAlive(true);
+
+        b.clientConnector(new ReactorClientHttpConnector(httpClient));
+    });
 
     @Test
     void concurrentRequest() {
@@ -25,7 +45,7 @@ public class Lec01HttpConnectionPoolingTest extends AbstractWebclient{
                 .verify();
     }
 
-    private Mono<ProductDto> getProduct(Integer id){
+    private Mono<ProductDto> getProduct(Integer id) {
         return client.get()
                 .uri("/product/{id}", 1)
                 .retrieve()
